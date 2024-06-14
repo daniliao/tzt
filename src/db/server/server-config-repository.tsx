b525b79132cbc02b9faa5b9c86060e9eaf088c14
Piotr Karwatka <piotr@catchthetornado.com>
2024-06-14 17:36:10 +0000

    [feat] config repo, health record and health record attachment schema

--- src/db/server/server-config-repository.tsx
+++ src/db/server/server-config-repository.tsx
@@ -1,21 +1,22 @@
 import { BaseRepository } from "../base-repository"
 import { Config } from "../models";
-import { db } from '@/db/server/db-provider'
+import { db, getCurrentTS } from '@/db/server/db-provider'
 import { config } from "./schema";
 import { eq } from "drizzle-orm/sql";
 
 export default class ServerConfigRepository extends BaseRepository<Config> {
 
-    // create a new patinet
+    // update config
     update(query:Record<string, any>, item: Config): Promise<Config> {        
 
         if(!query.***REMOVED***) throw new Error('Please set the query.***REMOVED*** to update the proper config variable');
 
-        let existingConfig = db.select({ ***REMOVED***: config.***REMOVED***, value: config.value}).from(config).where(eq(config.***REMOVED***, query.***REMOVED***)).get()
+        let existingConfig = db.select({ ***REMOVED***: config.***REMOVED***, value: config.value, updatedAt: config.updatedAt}).from(config).where(eq(config.***REMOVED***, query.***REMOVED***)).get()
         if (!existingConfig) {
             existingConfig = db.insert(config).values(item).returning().get()
         }
         existingConfig.value = item.value
+        existingConfig.updatedAt = getCurrentTS()
         db.update(config).set(existingConfig).where(eq(config.***REMOVED***, query.***REMOVED***)).run();
         return Promise.resolve(existingConfig as Config)   
     }
