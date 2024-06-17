a38855c94a016c688c469e800b2686630d35fc45
Piotr Karwatka <piotr@catchthetornado.com>
2024-06-17 11:31:19 +0000

    [feat] userConfig hook + patient-record refactor

---
+++ src/lib/useConfig.ts
@@ -0,0 +1,24 @@
+import { Config } from '@/db/models';
+import { useEffect, useState } from 'react';
+
+const useConfig = (): { configs: Config[], getByKey: (string) => any } => {
+    const [configs, setConfigs] = useState<Config[]>([]);
+
+    useEffect(() => {
+        const fetchConfigs = async () => {
+            try {
+                const response = await fetch('/***REMOVED***/config');
+                const data = await response.json();
+                setConfigs(data);
+            } catch (error) {
+                console.error('Error fetching configs:', error);
+            }
+        };
+
+        fetchConfigs();
+    }, []);
+
+    return { configs, getByKey: (***REMOVED***: string) => configs.find((config) => config.***REMOVED*** === ***REMOVED***) }
+};
+
+export default useConfig;
