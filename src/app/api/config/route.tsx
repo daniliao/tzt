3630f05f3752d521482ff0823a422d02bfc6e0e3
Piotr Karwatka <piotr@catchthetornado.com>
2024-06-17 14:55:57 +0000

    [feat] yup to zed migration, models rename to DTO's

--- src/app/api/config/route.tsx
+++ src/app/api/config/route.tsx
@@ -1,11 +1,11 @@
-import { Config, configSchema } from "@/db/models";
+import { ConfigDTO, configDTOSchema } from "@/db/models";
 import ServerConfigRepository from "@/db/server/server-config-repository";
 import { genericGET, genericPUT } from "@/lib/generic-***REMOVED***";
 
 export async function PUT(request: Request) {
-    return genericPUT<Config>(request, configSchema, new ServerConfigRepository(), '***REMOVED***');
+    return genericPUT<ConfigDTO>(request, configDTOSchema, new ServerConfigRepository(), '***REMOVED***');
 }
 
 export async function GET(request: Request) {
-    return genericGET<Config>(request, new ServerConfigRepository());
+    return genericGET<ConfigDTO>(request, new ServerConfigRepository());
 }
