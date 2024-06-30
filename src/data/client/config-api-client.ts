a01fa1e4290cf7ec97f723e160dc468a679075b2
Piotr Karwatka <piotr@catchthetornado.com>
2024-06-30 15:01:09 +0000

    [chore] promises fix

--- src/data/client/config-api-client.ts
+++ src/data/client/config-api-client.ts
@@ -1,8 +1,6 @@
 import { ConfigDTO } from "../dto";
 import { ApiClient, ApiEncryptionConfig } from "./base-***REMOVED***-client";
 
-export type GetConfigResponse = ConfigDTO[];
-
 export type PutConfigRequest = ConfigDTO;
 
 export type PutConfigResponseSuccess = {
@@ -29,6 +27,6 @@ export class ConfigApiClient extends ApiClient {
     }
   
     async put(config: PutConfigRequest): Promise<PutConfigResponse> {
-      return this.request<PutConfigResponse>('/***REMOVED***/config', 'PUT', config);
+      return this.request<PutConfigResponse>('/***REMOVED***/config', 'PUT', config) as Promise<PutConfigResponse>;
     }
   }
