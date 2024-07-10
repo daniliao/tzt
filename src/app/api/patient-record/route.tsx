542eb489446b6cbe25bd19ab30e7a9971aab4bbe
Piotr Karwatka <piotr@catchthetornado.com>
2024-07-10 21:45:52 +0000

    fixed issue [feat] Refactor patient-record-uploader for re-usable purposes #23

--- src/app/api/patient-record/route.tsx
+++ src/app/api/patient-record/route.tsx
@@ -4,6 +4,9 @@ import { genericGET, genericPUT } from "@/lib/generic-***REMOVED***";
 
 export async function PUT(request: Request) {
     const ***REMOVED***Result = await genericPUT<PatientRecordDTO>(await request.json(), patientRecordDTOSchema, new ServerPatientRecordRepository(), 'id');
+    if(***REMOVED***Result.status === 200) {
+        // TODO: update the encrypted_attachments.assigned_to field adding this record to the list
+    }
     return Response.json(***REMOVED***Result, { status: ***REMOVED***Result.status });
 
 }
