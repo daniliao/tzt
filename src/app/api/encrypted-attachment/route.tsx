8747e3affbf4673a3f54515c4beaad5fa32d4223
Piotr Karwatka <piotr@catchthetornado.com>
2024-07-11 04:50:52 +0000

    [feat] attachment assigments client side - done

--- src/app/api/encrypted-attachment/route.tsx
+++ src/app/api/encrypted-attachment/route.tsx
@@ -1,5 +1,5 @@
 import { EncryptedAttachmentDTO, EncryptedAttachmentDTOSchema } from "@/data/dto";
-import EncryptedAttachmentRepository from "@/data/server/server-encryptedattachment-repository";
+import ServerEncryptedAttachmentRepository from "@/data/server/server-encryptedattachment-repository";
 import { genericGET, genericPUT } from "@/lib/generic-***REMOVED***";
 import { StorageService } from "@/lib/storage-service";
 import { getErrorMessage } from "@/lib/utils";
@@ -13,25 +13,26 @@ export async function PUT(request: Request) {
     let ***REMOVED***Result = await genericPUT<EncryptedAttachmentDTO>(
         JSON.parse(formData.get("attachmentDTO") as string),
         EncryptedAttachmentDTOSchema,
-        new EncryptedAttachmentRepository(),
+        new ServerEncryptedAttachmentRepository(),
         'id'
     );
     if (***REMOVED***Result.status === 200) { // validation went OK, now we can store the file
-        try {
-            const savedAttachment: EncryptedAttachmentDTO = ***REMOVED***Result.data as EncryptedAttachmentDTO;
-            const file = formData.get("file") as File;
-            // TODO: move to a separate storage service
-            storageService.saveAttachment(file, savedAttachment.storageKey);
-        } catch (e) {
-            console.error("Error saving attachment", e);
-            ***REMOVED***Result.status = 500;
-            ***REMOVED***Result.message = getErrorMessage(e);
-            ***REMOVED***Result.error = e;
+        if (formData.get("file")) { // file could be not uploaded in case of metadata update
+            try {
+                const savedAttachment: EncryptedAttachmentDTO = ***REMOVED***Result.data as EncryptedAttachmentDTO;
+                const file = formData.get("file") as File;
+                storageService.saveAttachment(file, savedAttachment.storageKey);
+            } catch (e) {
+                console.error("Error saving attachment", e);
+                ***REMOVED***Result.status = 500;
+                ***REMOVED***Result.message = getErrorMessage(e);
+                ***REMOVED***Result.error = e;
+            }
         }
     }
     return Response.json(***REMOVED***Result, { status: ***REMOVED***Result.status });
 }
 
 export async function GET(request: Request) {
-    return Response.json(await genericGET<EncryptedAttachmentDTO>(request, new EncryptedAttachmentRepository()));
+    return Response.json(await genericGET<EncryptedAttachmentDTO>(request, new ServerEncryptedAttachmentRepository()));
 }
