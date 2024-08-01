72b7a7a6abf1f678df3659c6cdeeb0ec61ba5edc
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-01 12:00:57 +0200

    fix: expiryDate fix + findOneFix

--- drizzle/0011_strange_glorian.sql
+++ drizzle/0011_strange_glorian.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `***REMOVED***s` (
-	`***REMOVED***LocatorHash` text PRIMARY KEY NOT NULL,	
+	`***REMOVED***LocatorHash` text PRIMARY KEY NOT NULL,
+	`databaseIdHash` text NOT NULL,
 	`***REMOVED***Hash` text NOT NULL,
 	`***REMOVED***HashParams` text NOT NULL,
-	`databaseIdHash` text,
-	`encryptedMasterKey` text,
+	`encryptedMasterKey` text NOT NULL,
 	`acl` text,
 	`extra` text,
-	`expiryDate` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`expiryDate` text DEFAULT CURRENT_TIMESTAMP,
 	`updatedAt` text DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
--- src/data/server/server-key-repository.tsx
+++ src/data/server/server-key-repository.tsx
@@ -22,20 +22,20 @@ export default class ServerKeyRepository extends BaseRepository<KeyDTO> {
     // update config
     async upsert(query:Record<string, any>, item: KeyDTO): Promise<KeyDTO> {        
         const db = (await this.db());
-        let existingKey = db.select({ ***REMOVED***Hash: ***REMOVED***s.***REMOVED***Hash, databaseIdHash: ***REMOVED***s.databaseIdHash, updatedAt: ***REMOVED***s.updatedAt, extra: ***REMOVED***s.extra, acl: ***REMOVED***s.acl, expiryDate: ***REMOVED***s.expiryDate}).from(***REMOVED***s).where(eq(***REMOVED***s.***REMOVED***Hash, query['***REMOVED***Hash'])).get() as KeyDTO
+        let existingKey = db.select({ ***REMOVED***LocatorHash: ***REMOVED***s.***REMOVED***LocatorHash, ***REMOVED***Hash: ***REMOVED***s.***REMOVED***Hash, databaseIdHash: ***REMOVED***s.databaseIdHash, updatedAt: ***REMOVED***s.updatedAt, extra: ***REMOVED***s.extra, acl: ***REMOVED***s.acl, expiryDate: ***REMOVED***s.expiryDate}).from(***REMOVED***s).where(eq(***REMOVED***s.***REMOVED***LocatorHash, query['***REMOVED***LocatorHash'])).get() as KeyDTO
         if (!existingKey) {
             existingKey = await this.create(item)
         } else {
             existingKey = item
             existingKey.updatedAt = getCurrentTS()
-            db.update(***REMOVED***s).set(existingKey).where(eq(***REMOVED***s.***REMOVED***Hash, query['***REMOVED***Hash'])).run();
+            db.update(***REMOVED***s).set(existingKey).where(eq(***REMOVED***s.***REMOVED***LocatorHash, query['***REMOVED***LocatorHash'])).run();
         }
         return Promise.resolve(existingKey as KeyDTO)   
     }
 
     async delete(query: IFilter): Promise<boolean> {
         const db = (await this.db());
-        return db.delete(***REMOVED***s).where(eq(***REMOVED***s.***REMOVED***Hash, query['***REMOVED***Hash'])).run()
+        return db.delete(***REMOVED***s).where(eq(***REMOVED***s.***REMOVED***LocatorHash, query['***REMOVED***LocatorHash'])).run()
     }
 
     async findAll(query: KeysQuery): Promise<KeyDTO[]> {
@@ -50,6 +50,9 @@ export default class ServerKeyRepository extends BaseRepository<KeyDTO> {
             if(query.filter.***REMOVED***Hash){
                 dbQuery.where(eq(***REMOVED***s.***REMOVED***Hash, query.filter.***REMOVED***Hash))
             }
+            if(query.filter.***REMOVED***LocatorHash){
+                dbQuery.where(eq(***REMOVED***s.***REMOVED***LocatorHash, query.filter.***REMOVED***LocatorHash))
+            }            
         }
 
         return Promise.resolve(dbQuery.all() as KeyDTO[])
