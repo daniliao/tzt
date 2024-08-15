9ecea289730ec98ca37dcd0a671c65a53fdd7699
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-15 16:28:08 +0200

    feat #81, #80, #75

--- src/components/patient-record-form.tsx
+++ src/components/patient-record-form.tsx
@@ -142,7 +142,7 @@ export default function PatientRecordForm({ patient }: { patient?: Patient }) {
       if(savedPatientRecord?.id) // if patient record is saved successfully
       {
          const eaac = new EncryptedAttachmentApiClient('', dbContext, {
-          ***REMOVED***Key: dbContext,
+          ***REMOVED***Key: dbContext?.masterKey,
           useEncryption: true
         });
         uploadedAttachments?.forEach(async (attachmentToUpdate) => {
