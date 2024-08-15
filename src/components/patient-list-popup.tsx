6398ad3652be4db6cee48df7d995375c9e4bd336
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-15 17:17:10 +0200

    [fix] ui fixes deleting patients

--- src/components/patient-list-popup.tsx
+++ src/components/patient-list-popup.tsx
@@ -46,7 +46,7 @@ export default function PatientListPopup() {
                 ) : (
                   (patientsContext?.patients.length > 0) ?
                     patientsContext?.patients.map((patient, index) => (
-                      <PatientItem onClick={(e) => { e.preventDefault(); patientsContext?.setPatientListPopup(false); patientsContext.setCurrentPatient(patient); }} ***REMOVED***={index} patient={patient} selected={patientsContext?.currentPatient?.id === patient.id} />
+                      <PatientItem ***REMOVED***={index} patient={patient} selected={patientsContext?.currentPatient?.id === patient.id} />
                     ))
                     : (
                       <NoRecordsAlert title="No patients found">
